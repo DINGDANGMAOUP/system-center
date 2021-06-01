f99d12d98042c7aca42841bddc78bc1ecdb562c0
kuroneko <964621334@qq.com>
2021-06-01 17:03:50 +0800

    更新数据表

--- system-center/db/system_center.sql
+++ system-center/db/system_center.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80025
  File Encoding         : 65001
 
- Date: 27/05/2021 17:15:08
+ Date: 01/06/2021 17:03:02
 */
 
 SET NAMES utf8mb4;
@@ -30,13 +30,15 @@ CREATE TABLE `system_data_source`  (
   `driver` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '数据库连接驱动',
   `create_at` datetime NULL DEFAULT NULL COMMENT '创建时间',
   `modified_at` datetime NULL DEFAULT NULL COMMENT '更新时间',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+  `def` bit(1) NULL DEFAULT NULL COMMENT '是否默认数据源',
+  `db_type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '数据库类型',
+  PRIMARY KEY (`id`, `source_name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of system_data_source
 -- ----------------------------
-INSERT INTO `system_data_source` VALUES (1, 'mysql', 'root', 'root', 'jdbc:mysql://localhost:3306/kuroneko?serverTimezone=UTC', 'com.mysql.cj.jdbc.Driver', NULL, NULL);
-INSERT INTO `system_data_source` VALUES (2, 'sqlserver', 'sa', 'Dzh970102', 'jdbc:sqlserver://localhost:1433;DatabaseName=YinLuDWQAS', 'com.microsoft.sqlserver.jdbc.SQLServerDriver', NULL, NULL);
+INSERT INTO `system_data_source` VALUES (1, 'kuroneko', 'root', 'root', 'jdbc:mysql://localhost:3306/kuroneko?serverTimezone=UTC', 'com.mysql.cj.jdbc.Driver', '2021-05-28 08:11:20', '2021-05-31 09:11:49', b'1', 'mysql');
+INSERT INTO `system_data_source` VALUES (2, 'YinLuDWQAS', 'sa', 'Dzh970102', 'jdbc:sqlserver://localhost:1433;DatabaseName=YinLuDWQAS', 'com.microsoft.sqlserver.jdbc.SQLServerDriver', '2021-05-27 10:06:22', '2021-05-31 09:11:43', b'0', 'sqlserver');
 
 SET FOREIGN_KEY_CHECKS = 1;
